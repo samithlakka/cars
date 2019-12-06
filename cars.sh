fc85ea63b0b99215958ae5a838623164dab13f58
Samith Lakka <lakka@chapman.edu>
2019-12-06 10:20:11 -0800

    Added

---
+++ cars.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+#cars
+#Samith Lakka
+
+x=1
+while[ "$x" -eq "1" ]
+do
+	echo "Type 1 to add new car"
+	echo "Type 2 display list of cars"
+	echo "Type 3 to quit the program"
+	read usr
+	case "$usr" in
+		"1") echo "What year in the car"
+		read year
+		echo "What is the make of the car"
+		read make
+		echo "What is the model of the car"
+		read model
+		result="$year:$make:$model"
+		echo "$result" >> My_old_cars;;
+
+		"2")sort My_old_cars;;
